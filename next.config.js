3bc073469a6c83096f604530e09d6c93c88baa04
Rahultadmybrand <89353702+Rahultadmybrand@users.noreply.github.com>
2021-10-13 00:30:50 +0530

    Added japan

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ module.exports = {
   i18n: {
     // These are all the locales you want to support in
     // your application
-    locales: ['en-US', 'fr', 'nl-NL'],
+    locales: ['en-US', 'fr', 'nl-NL','ja'],
     // This is the default locale you want to be used when visiting
     // a non-locale prefixed path e.g. `/hello`
-    defaultLocale: 'fr',
+    defaultLocale: 'en-US',
     // This is a list of locale domains and the default locale they
     // should handle (these are only required when setting up domain routing)
     // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
@@ -22,8 +22,13 @@ module.exports = {
         domain: 'bassucascompany.fr',
         defaultLocale: 'fr',
         // an optional http field can also be used to test
-        // locale domains locally with http instead of https
-        http: true,
+        // locale domains locally with http instead of http
+      },
+{
+        domain: 'bassucascompany.ja',
+        defaultLocale: 'ja',
+        // an optional http field can also be used to test
+        // locale domains locally with http instead of http
       },
     ],
   },
